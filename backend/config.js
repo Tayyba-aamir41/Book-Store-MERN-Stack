9e1c28f51e6ad058d8e2d55005601004dd9554bf
Tayyba Aamir41 <tayybaaamir41a@gmail.com>
2025-04-12 18:00:57 +0500

    Initial commit - fresh start for Book Store MERN project

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+  'mongodb+srv://root:tayyba123@books-store-mern.dlsqtw8.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN';
+
+// This database will be deleted after tutorial
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',          // Your main HTML file
+    './src/**/*.{js,jsx,ts,tsx}', // All React components (or other JS/TS files)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
